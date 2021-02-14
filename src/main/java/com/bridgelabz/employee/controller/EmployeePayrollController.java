ae1ed7f31f2578b31aea480093e844b8779af356
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2021-02-14 09:57:16 +0530

    [Umesh]Fixed: error causing cross origin problem

--- src/main/java/com/bridgelabz/employee/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employee/controller/EmployeePayrollController.java
@@ -5,19 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.employee.model.EmployeeDO;
 import com.bridgelabz.employee.model.EmployeeEntity;
 import com.bridgelabz.employee.model.ResponseDo;
 import com.bridgelabz.employee.service.IEmployeePayrollService;
 
-
+@RestController
+@RequestMapping(value="/employee")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class EmployeePayrollController {
 
 	@Autowired
